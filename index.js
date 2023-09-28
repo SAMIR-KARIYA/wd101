7f71e5821ae87bf28f3b1e527087c20d30df69b4
SAMIR-KARIYA <110136419+SAMIR-KARIYA@users.noreply.github.com>
2023-09-29 01:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-window.onload = () => {
     const form = document.getElementById('registrationForm');
-    const table = document.getElementById('userTable');
-    const tbody = table.querySelector('tbody');
-
+    const table = document.getElementById('usertable');
+        
     let usersData = [];
     let storageData=localStorage.getItem('userData');
     if(storageData){
@@ -14,39 +12,40 @@ window.onload = () => {
             <td>${user.email}</td>
             <td>${user.password}</td>
             <td>${user.dob}</td>
-            <td>${user.acceptTerms ? 'Yes' : 'No'}</td>
-        `;
-        tbody.appendChild(newRow);
+            <td>${user.at ? 'Yes' : 'No'}</td>`;
+
+        table.appendChild(newRow);
         });
     }
-    function submitForm() {
-        // event.preventDefault();
+    function submitdata() {
+        console.log("Test");
         const dob = new Date(document.getElementById('dob').value);
         const today = new Date();
         const min = 18, max=55;
         const age = today.getFullYear() - dob.getFullYear();
         if (age < min || age > max) {
             alert(`You must be between ${min} and ${max} years old to register.`);
-            return;
+            return false;
         }
         const userData = {
             name: document.getElementById('name').value,
             email: document.getElementById('email').value,
             password: document.getElementById('password').value,
             dob: document.getElementById('dob').value,
-            acceptTerms: document.getElementById('acceptTerms').checked
+            at: document.getElementById('at').checked
         };
         usersData.push(userData);
         localStorage.setItem('userData', JSON.stringify(usersData));
+
         const newRow = document.createElement('tr');
         newRow.innerHTML = `
             <td>${userData.name}</td>
             <td>${userData.email}</td>
             <td>${userData.password}</td>
             <td>${userData.dob}</td>
-            <td>${userData.acceptTerms ? 'Yes' : 'No'}</td> `;
-        tbody.appendChild(newRow);
+            <td>${userData.at ? 'Yes' : 'No'}</td> `;
+
+        table.appendChild(newRow);
         form.reset();
-    });
-    return true;
-}
+        return true;
+    }
